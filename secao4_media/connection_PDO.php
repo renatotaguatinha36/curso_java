5a775d62620dd57bed10ee9891aeaf534bb29ec6
renatoguara2020 <renatoguara2020@gmail.com>
2025-04-27 12:23:46 -0300

    ok

--- secao4_media/connection_PDO.php
+++ secao4_media/connection_PDO.php
@@ -6,7 +6,7 @@ $password = "password";
 
 try {
 
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO ("mysql:host=$servername;dbname=myDB", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
