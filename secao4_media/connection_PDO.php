e19ae010b0bf82f9053b203cd0ee51ca6ff081f9
renatoguara2020 <renatoguara2020@gmail.com>
2025-05-08 11:18:00 -0300

    ok

--- secao4_media/connection_PDO.php
+++ secao4_media/connection_PDO.php
@@ -11,6 +11,7 @@ try {
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   echo "Connected successfully";
+  $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
   $stmt->bindParam(':firstname', $firstname);
   $stmt->bindParam(':lastname', $lastname);
   $stmt->bindParam(':email', $email);
